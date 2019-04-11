fc3758afdd1172a38bffe9b70750c767a90e52c4
Roque <roquealbertosoto@gmail.com>
2019-04-11 18:47:35 -0400

    Trying to edit the same line

--- gitPractice.java
+++ gitPractice.java
@@ -2,7 +2,8 @@ public class gitPractice {
 
   public static void main(String[] args) {
 	  
-	System.out.prinln("Welcome " + args[0] +"! Feel free to edit this as you see fit.");
+	System.out.prinln("Welcome " + args[0] + " and " + args[1] 
+	"! Feel free to edit this as you see fit.");
 	
 	System.out.println("You can run this code by editting your configurations before\n"+
 		"and adding args that this class can take as input arguments");
