534861223c2249bb1e059d25bd979f88ed63e3ef
Roque <roquealbertosoto@gmail.com>
2019-04-11 18:50:23 -0400

    Update gitPractice.java

--- gitPractice.java
+++ gitPractice.java
@@ -2,7 +2,7 @@ public class gitPractice {
 
   public static void main(String[] args) {
 	  
-	System.out.prinln("Welcome " + args[0] + " and " + args[1] 
+	System.out.prinln("Welcome " + args[0] + " and " + args[1] + " and " + args[2] +
 	"! Feel free to edit this as you see fit.");
 	
 	System.out.println("You can run this code by editting your configurations before\n"+
