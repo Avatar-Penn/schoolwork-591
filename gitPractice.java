2311383f5198f44e6b6ac0c8809d2efc17300c30
Avatar-Penn <49504936+Avatar-Penn@users.noreply.github.com>
2019-04-11 18:51:26 -0400

    Update gitPractice.java

--- gitPractice.java
+++ gitPractice.java
@@ -5,7 +5,7 @@ public class gitPractice {
 	System.out.prinln("Velcome " + args[0] + " and " + args[1] 
 	"! Feel free to edit this document as you see fit.");
 	
-	System.out.println("You can run this code by editting your configurations before\n"+
+	System.out.println("You cannot run this code by editting your configurations before\n"+
 		"and adding args that this class can take as input arguments");
     }
 }
