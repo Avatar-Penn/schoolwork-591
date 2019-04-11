2b43eee17ab40fce014bac02a7190ccc64c2985c
Avatar-Penn <49504936+Avatar-Penn@users.noreply.github.com>
2019-04-11 18:44:30 -0400

    Update gitPractice.java

--- gitPractice.java
+++ gitPractice.java
@@ -2,7 +2,7 @@ public class gitPractice {
 
   public static void main(String[] args) {
 	  
-	System.out.prinln("Welcome " + args[0] +"! Feel free to edit this as you see fit.");
+	System.out.prinln("Velcome " + args[0] +"! Feel free to edit this as you see fit.");
 	
 	System.out.println("You can run this code by editting your configurations before\n"+
 		"and adding args that this class can take as input arguments");
