2bc648cec023d758140766405d1accb1785eea56
Avatar-Penn <49504936+Avatar-Penn@users.noreply.github.com>
2019-04-11 18:50:13 -0400

    Update gitPractice.java

--- gitPractice.java
+++ gitPractice.java
@@ -2,7 +2,7 @@ public class gitPractice {
 
   public static void main(String[] args) {
 	  
-	System.out.prinln("Welcome " + args[0] + " and " + args[1] 
+	System.out.prinln("Velcome " + args[0] + " and " + args[1] 
 	"! Feel free to edit this document as you see fit.");
 	
 	System.out.println("You can run this code by editting your configurations before\n"+
