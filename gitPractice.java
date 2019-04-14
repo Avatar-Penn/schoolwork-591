2084c61eaa709a9300beaf80fbd5f6afb56f46ee
Avatar-Penn <49504936+Avatar-Penn@users.noreply.github.com>
2019-04-14 14:54:58 -0400

    Update gitPractice.java

--- gitPractice.java
+++ gitPractice.java
@@ -4,7 +4,7 @@ public class gitPractice {
   public static void main(String[] args) {
 	  
 	System.out.prinln("Velcome " + args[0] + " and " + args[1] 
-	"! Feel free to edit this document as you see fit.");
+	"! Feel free to edit this document as you see fit.  changed by AVATAR");
 	
 	System.out.println("You cannot run this code by editting your configurations before\n"+
 		"and adding args that this class can take as input arguments");
